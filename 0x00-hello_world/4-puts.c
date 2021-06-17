fdbfeadb4bbe135bdf9e23de8ea7f024af8ede06
Marisol Lozano <3365@holbertonschool.com>
2021-06-17 15:48:59 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
+
 #include <stdio.h>
 
 int main(void)
-/*Imprimiendo con el comando puts.*/
+/* Imprimiendo con el comando puts.*/
 
 {
   
-puts("\"Programming is like building a multilingual puzzle")
+puts("\""Programming is like building a multilingual puzzle)
 return (0);
  
 }
