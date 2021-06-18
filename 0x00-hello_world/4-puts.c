f2d27282394b6dfb4a046adcd1ed8296810246f5
MarisolLoto <85497672+MarisolLoto@users.noreply.github.com>
2021-06-17 19:07:33 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*main - funcion principal*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
