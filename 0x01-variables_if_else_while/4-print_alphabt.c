c35a204e62c278c5e5790de5846a8154d2c13011
Marisol Lozano <3365@holbertonschool.com>
2021-06-18 19:04:07 -0700

    Hola que hace?

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+char i = 'a';
+char j = 'z';
+while (i <= j)
+{
+if(i != 'e' && i != 'q')
+{
+putchar(i);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
