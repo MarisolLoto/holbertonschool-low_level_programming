57ffe7cbd1b406b63e6f9d43e5801c7d16e9630c
Marisol Lozano <3365@holbertonschool.com>
2021-06-18 20:33:34 -0700

    Hola que hace?

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+char i = 48;
+while (i <= 57)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
