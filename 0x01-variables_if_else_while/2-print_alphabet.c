947db597f5f5498bad53bb030c86d82eeffabff6
Marisol Lozano <3365@holbertonschool.com>
2021-06-18 17:27:35 -0700

    Hola que hace?

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+* Return: 0 on success
+*/
+int main(void)
+{
+char alfabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+for (int i=0; i<26;i++)
+{
+putchar(alfabet[i]);
+}
+putchar('\n');
+return (0);
+}
