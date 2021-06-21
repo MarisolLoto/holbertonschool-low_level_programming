4dfcd7be29719dd0d30d94909bcaaba5f812282a
Marisol Lozano <3365@holbertonschool.com>
2021-06-21 10:02:33 -0700

    Holberton imprimir

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,10 @@
+#include <holberton.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+_putchar("Holerton");
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <holberton.h>
+/**
+ * main - Determine if the last digit of a random number is
+ * greater than or less than 5, or is zero..
+(*
+* Return: 0 on success
+*/
+void print_alphabet(void);
+{
+char i = 'a';
+char j = 'z';
+while (i <= j)
+{
+_putchar(i);
+}
+i++;
+}
+_putchar('\n');
+return (0);
+}
