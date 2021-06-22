159caa90eec04a33f92b95a9f28cf6dea7076dad
Marisol Lozano <3365@holbertonschool.com>
2021-06-22 15:50:24 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_last_digit - imprimir el ultimo dig
+* Return: 1 if letter, 0 otherwise
+**/
+int print_last_digit(int);
+{
+int f;
+f = c % 10;
+if (f < 0)
+{
+f = f * -1;
+}
+return(f);
+}
