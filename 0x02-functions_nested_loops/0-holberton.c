f8a010bc38f5141cf567c9c8c2777baee0c04ee0
Marisol Lozano <3365@holbertonschool.com>
2021-06-22 09:21:18 -0700

    imprimir

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,17 +3,17 @@
  *
  * Return: Always 0.
 */
-int main(void)
+int main()
 {
-_putchar("H");
-_putchar("o");
-_putchar("l");
-_putchar("b");
-_putchar("e");
-_putchar("r");
-_putchar("t");
-_putchar("o");
-_putchar("n");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
 return (0);
 }
