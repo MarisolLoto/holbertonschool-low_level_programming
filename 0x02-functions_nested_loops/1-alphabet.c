259a144d8de236cb525998778c449c424e965ba6
Marisol Lozano <3365@holbertonschool.com>
2021-06-22 12:35:36 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
 /**
-* print_alphabet 
+* print_alphabet
 * Return: 0
 */
 void print_alphabet(void)
 {
-char i = 'a';
-char j = 'z';
-while (i <= j)
+char i = 'z';
+while (i >= 'a')
 {
 _putchar(i);
-}
 i++;
 }
 _putchar('\n');
