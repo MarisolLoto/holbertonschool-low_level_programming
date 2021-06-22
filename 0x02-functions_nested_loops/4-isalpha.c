a915551b076da60a21b78a0cee1d2cc5724df0a2
Marisol Lozano <3365@holbertonschool.com>
2021-06-22 14:54:59 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* _isalpha - returns - check the code for Holberton School students.
+* @c: no entiendo este caracter
+* Return:
+*/
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
