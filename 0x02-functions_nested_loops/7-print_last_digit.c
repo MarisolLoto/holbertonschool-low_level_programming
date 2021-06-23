d09c5c18111e8928b1d8c56672021fc7d328755f
Marisol Lozano <3365@holbertonschool.com>
2021-06-22 18:21:18 -0700

    lo

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,12 @@
 /**
 * print_last_digit - imprimir el ultimo dig
 * @n: vlor abs
-* Return: print last d 
+* Return: print last d
 */
 int print_last_digit(int n)
 {
 n = n % 10;
 if (n < 0)
-{
-_putchar(n = n * -1);
-}
-return(n);
+(n = n * -1)
+return (n);
 }
