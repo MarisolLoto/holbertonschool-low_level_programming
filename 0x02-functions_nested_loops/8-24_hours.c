d6a032c105dacf58b778b9888681961dfc216b2e
Marisol Lozano <3365@holbertonschool.com>
2021-06-23 20:48:36 -0700

    nested

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* jack_bauer - print the numbers from to 98
+* 
+*
+* Return: void returns nothing.
+*/
+void jack_bauer(void)
+{
+int i;
+int j;
+for (i = 0; i < 24 ; i++)
+{
+for (j = 0; j < 60; j++)
+{
+if (i < 10)
+{
+putchar('0');
+}
+printf("%d" ,i);
+putchar(':');
+if (j < 10)
+{
+putchar('0');
+}
+printf("%d" ,j);
+putchar('\n');
+}
+}
+}
