49682be60fddf4d05708af89b59a17277f0e29f6
Marisol Lozano <3365@holbertonschool.com>
2021-06-25 11:48:48 -0700

    putchar

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,12 +1,12 @@
-#include "holberton.h"
-
+#include <unistd.h>
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-print_most_numbers();
-return (0);
+	return (write(1, &c, 1));
 }
