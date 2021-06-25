e8041c143bda7d277f286965ca567294941b277c
Marisol Lozano <3365@holbertonschool.com>
2021-06-25 13:51:22 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n: integra
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+
+{
+	while (n > 0)
+	{
+		_putchar(95);
+		n--;
+	}
+	_putchar(10);
+}
