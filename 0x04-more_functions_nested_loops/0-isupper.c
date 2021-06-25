0bc54f9acbbbd5ea80837b0a46ec1c1edfaab752
Marisol Lozano <3365@holbertonschool.com>
2021-06-24 17:02:35 -0700

    isuu

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,13 +7,13 @@
  * Return: Always 0.
  */
 int _isupper(int c)
-int main(void)
 {
-	int c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
 }
