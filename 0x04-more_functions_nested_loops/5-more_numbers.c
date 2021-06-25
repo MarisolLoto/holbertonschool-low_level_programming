1aeb9901d83f65eb98ffe28e6af69d1ccad9236e
Marisol Lozano <3365@holbertonschool.com>
2021-06-25 13:46:45 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
 #include "holberton.h"
 
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 
 void more_numbers(void)
 {
-int c, d;
+	int c, d;
 
-for (d = 0; d <= 9; d++)
-{
-for (c = 0; c <= 14; c++)
-{
-if (c >= 10)
-_putchar('0' + c / 10);
-_putchar('0' + c % 10);
-}
-_putchar ('\n');
-}
+	for (d = 0; d <= 9; d++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+				_putchar('0' + c / 10);
+			_putchar('0' + c % 10);
+		}
+		_putchar ('\n');
+	}
 }
