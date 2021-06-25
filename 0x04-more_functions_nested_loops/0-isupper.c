3349d77eb1f18b725393c012348bbb1526f8cded
Marisol Lozano <3365@holbertonschool.com>
2021-06-24 17:16:08 -0700

    issuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _isupper : of an integer and returns a value of type int
+ * @c: alfabeth impri
  * Return: Always 0.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
