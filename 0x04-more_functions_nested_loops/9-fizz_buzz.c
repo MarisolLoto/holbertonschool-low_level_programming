219031201027a8641d8c4af813d7941c571ec943
Marisol Lozano <3365@holbertonschool.com>
2021-06-25 17:22:05 -0700

    cheched

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+* main: fizzbuzz
+*/
+
+int main(void)
+
+{
+int i = 1;
+while (i <= 100)
+{
+if (i == 100)
+{
+printf("Buzz\n");
+}
+else if (i % 5 == 0 && i % 3 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+printf("%d ", i);
+i++;
+}
+return (0);
+}
