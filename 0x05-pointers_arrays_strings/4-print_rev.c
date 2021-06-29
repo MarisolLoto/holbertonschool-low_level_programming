8ffbaa89ab0f97cafd3e14effc59095b6bb4d3ca
Marisol Lozano <3365@holbertonschool.com>
2021-06-28 22:06:49 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_rev - check the code for Holberton School students.
+ * @s: funtion
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	while (*s != 0)
+	{
+		s++;
+		len++;
+	}
+	for (; len >= 0;)
+	{
+		if (*s != '\0')
+		{
+			_putchar(*s);
+		}
+		s--;
+		len--;
+	}
+	_putchar('\n');
+}
