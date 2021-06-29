a26fc6e435f77891fc88e04b9dc054655ae32db1
Marisol Lozano <3365@holbertonschool.com>
2021-06-28 18:03:08 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
 /**
- * swap_int - cambia el valor de dos enteros
- * @ab: modificado
+ * swap_int - cambia de posion el valor de dos enteros
+ * @a: pointer
+ * @b: pointer
  * Return - entero
  */
 void swap_int(int *a, int *b);
 {
-swap_int(&a, &b);
+int t;
+t  = *b;
+*b = *a;
+*a = t;
 }
